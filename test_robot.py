2197e8fbd6332130fa959b091094ed8319642f8e
ydia530 <55938375+ydia530@users.noreply.github.com>
2020-08-23 16:25:02 +1200

    Fixed bugs and added more tests

---
+++ test_robot.py
@@ -0,0 +1,161 @@
+import pytest
+
+from robot import Robot, Direction, IllegalMoveException
+
+
+@pytest.fixture
+def robot():
+    return Robot()
+
+
+def test_constructor(robot):
+    state = robot.state()
+
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
+def test_east_turn(robot):
+    robot.turn()
+
+    state = robot.state()
+    assert state['direction'] == Direction.EAST
+
+def test_south_turn(robot):
+    robot.turn()
+    robot.turn()
+
+    state = robot.state()
+    assert state['direction'] == Direction.SOUTH
+    
+def test_west_turn(robot):
+    robot.turn()
+    robot.turn()
+    robot.turn()
+
+    state = robot.state()
+    assert state['direction'] == Direction.WEST
+
+def test_north_turn(robot):
+    robot.turn()
+    robot.turn()
+    robot.turn()
+    robot.turn()
+
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+
+
+
+
+
+
+
+def test_illegal_move(robot):
+    robot.turn();
+    robot.turn();
+
+    with pytest.raises(IllegalMoveException):
+        robot.move()
+def test_illegal_move_west(robot):
+    robot.turn();
+    robot.turn();
+    robot.turn()
+    with pytest.raises(IllegalMoveException):
+        robot.move()
+def test_illegal_move_east(robot):
+    robot.turn();
+    while robot.state()['col'] != 10:
+        robot.move()
+
+    with pytest.raises(IllegalMoveException):
+        robot.move()
+def test_illegal_move_North(robot):
+    while robot.state()['row'] != 1:
+        robot.move()
+
+    with pytest.raises(IllegalMoveException):
+        robot.move()
+
+
+def test_move_north(robot):
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 9
+    assert state['col'] == 1
+
+def test_move_west(robot):
+    robot.turn()
+    robot.move()
+    robot.turn()
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 1
+def test_move_east(robot):
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 2
+def test_move_south(robot):
+    robot.move()
+    robot.turn()
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
+def test_back_track_without_history(robot):
+    robot.back_track()
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
+def test_back_track_with_one_move(robot):
+    robot.move()
+    robot.back_track()
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+def test_back_track_with_one_turn(robot):
+    robot.turn()
+    robot.back_track()
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+def test_back_track_with_mutiple_moves(robot):
+    robot.move()
+    robot.move()
+    robot.move()
+
+    robot.back_track()
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 8
+    assert state['col'] == 1
+
+def test_back_track_with_mutiple_moves_allback(robot):
+    robot.move()
+    robot.move()
+    robot.move()
+
+    robot.back_track()
+    robot.back_track()
+    robot.back_track()
+    state = robot.state()
+    assert state['direction'] == Direction.NORTH
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
